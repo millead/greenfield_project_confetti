b158d0e4fa17dba056b34cecd445e12b16df71f3
Apollo Miller <apollo.miller@yahoo.com>
2024-08-19 08:20:00 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Confetti from "react-confetti";
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Confetti />
+      <h1>Wow, I'm working with React on my machine!</h1>
+      <p>Is this what happiness feels like?</p>
+    </div>
+  );
+}
+
+export default App;
